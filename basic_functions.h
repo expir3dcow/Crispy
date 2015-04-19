6778e9d3ad9763714f0196188b1539cd0c7e234f
expir3dcow <expir3dcow@gmail.com>
2015-04-19 13:26:40 +0100

    Update basic_functions.h

--- basic_functions.h
+++ basic_functions.h
@@ -1,3 +1,28 @@
 #include <string>
 #include <string.h>
-void setVersionName(String version);
+
+
+string version_name = "Crispy v1.0";
+
+static string (*Name_Real)(void *p);
+
+string versionNameHook(){
+  return version_name;
+}
+
+
+//Defined functions
+
+
+
+namespace Crispy{
+
+  void setVersionName(String version);
+  
+  void setVersionName(String version){
+    version_name = version;
+    void* handle = dlsym(RTLD_DEFAULT,"_ZN6Common20getGameVersionStringEv");
+    mcpelauncher_hook(handle,(void*)&versionNameHook,(void**)&Name_Real);
+  }
+
+}
