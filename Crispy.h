e3a80c60fe60d322816f1b7e01789ae4512fc5b5
expir3dcow <expir3dcow@gmail.com>
2015-04-19 14:20:49 +0100

    Update Crispy.h

--- Crispy.h
+++ Crispy.h
@@ -1,9 +1,15 @@
+#ifndef Cripsy
+#define Crispy
+#endif
+
 #include <string>
 #include <string.h>
 #include <jni.h>
 #include <android/log.h>
 #include <dlfcn.h>
 #include "mcpelauncher.h"
+
+using namespace std;
 
 string version_name = "Crispy v1.0";
 
@@ -14,15 +20,14 @@ string versionNameHook(){
 }
 
 
-//Defined functions
 
 
 
 namespace Crispy{
 
-  void setVersionName(String version);
+  void setVersionName(string version);
   
-  void setVersionName(String version){
+  void setVersionName(string version){
     version_name = version;
     void* handle = dlsym(RTLD_DEFAULT,"_ZN6Common20getGameVersionStringEv");
     mcpelauncher_hook(handle,(void*)&versionNameHook,(void**)&Name_Real);
